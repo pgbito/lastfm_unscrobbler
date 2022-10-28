aa0193b44123b369a55e3ef0756fdd6b0207120d
pgbito <84261118+pgbito@users.noreply.github.com>
2022-10-28 17:07:16 -0600

    Create run.js

---
+++ run.js
@@ -0,0 +1,40 @@
+
+
+const process = require('process')
+
+const [bn, ...argv] = Array.from(process.argv.slice(1))
+
+const usage=`
+
+                 _                                               
+|  _.  _ _|_   _|_ ._ _        ._   _  _ ._ _  |_  |_  |  _  ._ 
+| (_| _>  |_    |  | | |   |_| | | _> (_ | (_) |_) |_) | (/_ |   by pgbito
+                                               
+
+Usage: ${ require('path').basename(bn)} {login, unscrobble}`
+
+
+switch (argv[0]) {
+    case "login":
+        try {
+            require('./login').exec()
+        } catch (error) {
+            console.log(error)
+        }
+        break;
+    case "unscrobble":
+        try {
+            require('./unscrobbler').exec()
+        } catch (error) {
+            console.log(error.message)
+           
+        }
+        break;
+    case undefined:
+        console.log(usage)
+        break;
+    default:
+        console.log(usage)
+        break;
+}
+process.exit(0)
